89eec4affa9acfa5fda8b0ae8fd3225e31386f66
Jose Alonso Solis Lemus <alonso.jasl@gmail.com>
2015-10-21 02:45:55 +0100

    Added script to test conv neural networks.

--- cnnPool.m
+++ cnnPool.m
@@ -48,35 +48,51 @@ end
 
 [convDim1, convDim2, numFilters, numImages] = size(Fconv);
 poolDimension = [fix(convDim1/poolSize) fix(convDim2/poolSize)];
+%poolDimension = [poolSize poolSize];
 
-Fpool = zeros(poolSize, poolSize,numFilters, numImages);
+Fpool = zeros(fix(convDim1/poolSize), fix(convDim2/poolSize),...
+    numFilters, numImages);
 
-rowsizes = poolDimension(1)*ones(poolSize, 1);
-colsizes = poolDimension(2)*ones(poolSize, 1);
-
-missingRows = convDim1 - sum(rowsizes);
-missingCols = convDim2 - sum(colsizes);
-if missingRows > 0 
-    for i=1:missingRows
-        rowsizes(i) = rowsizes(i) + 1;
-    end
-end
-if missingCols > 0 
-    for j=1:missingCols
-        colsizes(i) = colsizes(i) + 1;
-    end
-end
-
-for i=1:numFilters
-    for j=1:numImages
-        C = mat2cell(Fconv(:,:,i,j), rowsizes,colsizes);
-        [ROWS, COLS] = size(C);
-        for row=1:ROWS
-            for col=1:COLS
-                Fpool(row,col,i,j) = feval(whichPool,C{row,col}(:));
+he=1:poolSize:poolDimension(1)-1;
+wi=1:poolSize:poolDimension(2)-1;
+        
+for h=1:length(he)-1
+    for w=1:length(wi)-1
+        for i=1:numFilters
+            for j=1:numImages
+                A = Fconv(he(h):he(h+1)-1,wi(w):wi(w+1)-1,i,j);
+                Fpool(h,w,i,j) = feval(whichPool, A(:));
             end
         end
     end
 end
+
+
+% rowsizes = poolDimension(1)*ones(poolSize, 1);
+% colsizes = poolDimension(2)*ones(poolSize, 1);
+% missingRows = convDim1 - sum(rowsizes);
+% missingCols = convDim2 - sum(colsizes);
+% if missingRows > 0 
+%     for i=1:missingRows
+%         rowsizes(i) = rowsizes(i) + 1;
+%     end
+% end
+% if missingCols > 0 
+%     for j=1:missingCols
+%         colsizes(j) = colsizes(j) + 1;
+%     end
+% end
+% 
+% for i=1:numFilters
+%     for j=1:numImages
+%         C = mat2cell(Fconv(:,:,i,j), rowsizes,colsizes);
+%         [ROWS, COLS] = size(C);
+%         for row=1:ROWS
+%             for col=1:COLS
+%                 Fpool(row,col,i,j) = feval(whichPool,C{row,col}(:));
+%             end
+%         end
+%     end
+% end
 
         
---
+++ script_cnnPasspatchCNN.m
@@ -0,0 +1,48 @@
+% script file: 
+% Help me pass one 28X28 patch through the neural network with two conv
+% layers, two max pooling stages and two fully connected layers. 
+% 
+
+clc;
+
+filterDim = 5;
+numFilters = 12;
+poolsize = 2;
+
+W1 = randn(filterDim, filterDim, numFilters);
+b1 = randn(numFilters, 1);
+W2 = randn(filterDim, filterDim, 1);
+b2 = randn;
+
+A1 = rand(64,192);
+A2 = rand(2,64);
+
+X = imread('peppers.png');
+X = double(X);
+X = rgb2gray(X);
+
+x1 = double(X(110:137,65:92))/max(double(X(:)));
+y1 = 1;
+x2 = double(X(65:92,110:137))/max(double(X(:)));
+y2 = 0;
+
+[Fconv1] = cnnConvolve(filterDim,numFilters,x1,W1,b1);
+[Pool1] = cnnPool(poolsize,Fconv1,'mean');
+[Fconv2] = cnnConvolve(filterDim, 1,Pool1,W2,b2);
+[Pool2] = cnnPool(poolsize,Fconv2,'mean');
+
+netIn = Pool2(:);
+a1 = sigmoid(A1*netIn);
+a2 = sigmoid(A2*a1);
+
+%% After having this with a dummy patch
+dnds = '/Users/jsolisl/Documents/propio/PhD/SW/matlab/CNN/';
+imname = 'N2DHGOWT1_0.tif';
+bimname = 'GT_N2DHGOWT1_0.tif';
+
+[X, xatt] = readParseInput(strcat(dnds,imname));
+[Xb, batt] = readParseInput(strcat(dnds, bimname));
+
+[cells] = getTrainingImages(X,Xb);
+Cell = cells(:,:,1);
+
